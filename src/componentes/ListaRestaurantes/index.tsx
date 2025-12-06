11351d8cee86f5cc93e4a6bc023d64f81da5ce6f
“Vinicius <viniciussilva707@gmail.com>
2025-12-06 14:44:02 -0300

    Enhance ListaRestaurantes component to support pagination by adding 'ver mais' button for loading additional restaurant data from API

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -3,20 +3,37 @@ import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
 import axios from "axios";
+import { IPaginacao } from "../../interfaces/IPaginacao";
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState("");
 
   useEffect(() => {
     axios
-      .get("http://localhost:8000/api/v1/restaurantes/")
+      .get<IPaginacao<IRestaurante>>(
+        "http://localhost:8000/api/v1/restaurantes/"
+      )
       .then((resposta) => {
         setRestaurantes(resposta.data.results);
+        setProximaPagina(resposta.data.next);
       })
       .catch((erro) => {
         console.log(erro);
       });
   }, []);
+
+  const verMais = () => {
+    axios
+      .get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then((resposta) => {
+        setRestaurantes([...restaurantes, ...resposta.data.results]);
+        setProximaPagina(resposta.data.next);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  };
 
   return (
     <section className={style.ListaRestaurantes}>
@@ -26,6 +43,7 @@ const ListaRestaurantes = () => {
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
+      {proximaPagina && <button onClick={verMais}>ver mais</button>}
     </section>
   );
 };

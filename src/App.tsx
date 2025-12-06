7ac4be21c41bf7c43e39f32a0f51042dfe519924
“Vinicius <viniciussilva707@gmail.com>
2025-12-06 15:36:57 -0300

    Add FormularioRestaurante component and route for creating new restaurants

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./paginas/Home";
 import VitrineRestaurantes from "./paginas/VitrineRestaurantes";
 import { AdministracaoRestaurantes } from "./paginas/Administracao/Restaurantes/AdministracaoRestaurantes";
+import FormularioRestaurante from "./paginas/Administracao/Restaurantes/FormularioRestaurante";
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
       <Route
         path="/admin/restaurantes"
         element={AdministracaoRestaurantes}
+      ></Route>
+      <Route
+        path="/admin/restaurantes/novo"
+        element={FormularioRestaurante}
       ></Route>
     </Routes>
   );

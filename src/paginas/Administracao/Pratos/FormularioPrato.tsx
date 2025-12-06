d94a017c63dfcc4f61e01023616a1e59f83faa62
“Vinicius <viniciussilva707@gmail.com>
2025-12-06 17:43:48 -0300

    Implement form submission for creating new dishes in FormularioPrato, including handling image uploads and resetting form fields upon successful submission.

--- src/paginas/Administracao/Pratos/FormularioPrato.tsx
+++ src/paginas/Administracao/Pratos/FormularioPrato.tsx
@@ -47,6 +47,35 @@ export const FormularioPrato = () => {
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
+    const formData = new FormData();
+
+    formData.append("nome", nomePrato);
+    formData.append("descricao", descricao);
+
+    formData.append("tag", tag);
+
+    formData.append("restaurante", restaurante);
+
+    if (imagem) {
+      formData.append("imagem", imagem);
+    }
+
+    http
+      .request({
+        url: "pratos/",
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        data: formData,
+      })
+      .then(() => {
+        setNomePrato("");
+        setDescricao("");
+        setTag("");
+        alert("Prato cadastrado com sucesso!");
+      })
+      .catch((erro) => console.log(erro));
   };
   return (
     <Box
@@ -87,7 +116,7 @@ export const FormularioPrato = () => {
             onChange={(evento) => setTag(evento.target.value)}
           >
             {tags.map((tag) => (
-              <MenuItem key={tag.id} value={tag.id}>
+              <MenuItem key={tag.id} value={tag.value}>
                 {tag.value}
               </MenuItem>
             ))}
